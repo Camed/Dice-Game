520245f57b60f4dcc66bc34427e319a05b56f5f0
Gabriel <camed.tf@gmail.com>
2016-05-19 17:15:22 +0200

    Add files via upload

---
+++ againstComputerWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading;
+namespace Dice_Game
+{
+
+    public partial class againstComputerWindow : Window
+    {
+        private Random r = new Random();
+
+        public againstComputerWindow()
+        {
+            InitializeComponent();
+        }
+
+            
+        int _totalUserPoints     = 0;
+        int _totalComputerPoints = 0;   
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            int randomValue = r.Next(1, 7);
+
+            if(randomValue == 1)
+            {
+                _totalUserPoints += 0;
+                throw_value_user.Content = "1";
+                MessageBox.Show("You've thrown 1! No points!");
+                ComputerTurn();
+            }
+            else
+            {
+                _totalUserPoints += randomValue;
+                throw_value_user.Content = randomValue.ToString();
+                new Thread(new ThreadStart(() => Thread.Sleep(1000))).Start();
+                if (_totalUserPoints >= 100)
+                { 
+                    MessageBox.Show("You won!");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Computer's turn");
+                    userPoints.Content = _totalUserPoints.ToString();
+                    ComputerTurn();
+                }
+                
+            }
+
+        }
+
+
+        public void ComputerTurn()
+        {
+
+          int randomValue = r.Next(1, 7);
+
+          if(randomValue == 1)
+          {
+                throw_value_computer.Content = "1";
+          }
+          else
+          {
+                _totalComputerPoints += randomValue;
+                throw_value_computer.Content = randomValue.ToString();
+                computerPoints.Content = _totalComputerPoints.ToString();
+                new Thread(new ThreadStart(() => Thread.Sleep(1000))).Start();     // wait one second until next throw and don't block GUI
+                if (_totalComputerPoints >= 100) { MessageBox.Show("Computer won!"); this.Close(); }
+          }
+        }
+
+    }
+}
---
+++ pvp.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dice_Game
+{
+    /// <summary>
+    /// Interaction logic for pvp.xaml
+    /// </summary>
+    public partial class pvp : Window
+    {
+
+        private Random r = new Random();
+
+        int _totalPointsP1 = 0;
+        int _totalPointsP2 = 0;
+
+        public pvp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            p1roll.IsEnabled = false;
+            p2roll.IsEnabled = true;
+
+            int randomValue = r.Next(1, 7);
+
+            p1label.Content = randomValue.ToString();
+
+            if(randomValue == 1)
+            {
+                _totalPointsP1 += 0;
+                MessageBox.Show("Player 1 thrown 1! No points added!");
+            }
+            else
+            {
+                _totalPointsP1 += randomValue;
+                if(_totalPointsP1 >= 100)
+                {
+                    MessageBox.Show("Player 1 won!");
+                    this.Close();
+                }
+                else p1total.Content = string.Format("Player 1 total points: {0}", _totalPointsP1);
+                
+            }
+            
+        }
+
+        private void p2roll_Click(object sender, RoutedEventArgs e)
+        {
+            p1roll.IsEnabled = true;
+            p2roll.IsEnabled = false;
+
+            int randomValue = r.Next(1, 7);
+
+            p2label.Content = randomValue.ToString();
+
+            if (randomValue == 1)
+            {
+                _totalPointsP2 += 0;
+                MessageBox.Show("Player 2 thrown 1! No points added!");
+            }
+            else
+            {
+                _totalPointsP2 += randomValue;
+
+                if(_totalPointsP2 >= 100)
+                {
+                    MessageBox.Show("Player2 won!");
+                    this.Close();
+                }
+                else p2total.Content = string.Format("Player 2 total points: {0}", _totalPointsP2);
+
+            }
+        }
+
+        
+    }
+}
